fbade0e1431c611f4b8e34a7949d665ec8044331
pierre danselme <danselmepierre@gmail.com>
2023-09-17 18:57:02 +0200

    update of migrations

--- db/migrate/20230914140222_add_columns_to_movies.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToMovies < ActiveRecord::Migration[7.0]
-  def change
-    add_column :movies, :title, :string
-    add_column :movies, :overview, :string
-    add_column :movies, :poster_url, :string
-    add_column :movies, :rating, :float
-  end
-end
--- db/migrate/20230914140953_add_columns_to_bookmarks.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToBookmarks < ActiveRecord::Migration[7.0]
-  def change
-    add_column :bookmarks, :comment, :string
-    add_reference :bookmarks, :movie, foreign_key: true
-    add_reference :bookmarks, :list, foreign_key: true
-  end
-end
